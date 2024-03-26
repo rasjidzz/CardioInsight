6a2ed14738b03d13310dd87d8dd880dff7d4d5ff
rasjidzz <mrisjads@gmail.com>
2024-03-26 23:46:16 +0700

    Upd : Initiate Login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,9 @@
+<?php 
+    class LoginController extends Controller{
+        public function index(){
+            $data = [
+                'title' => 'Login'
+            ];
+        }
+    }
+?>
