d3e16473a3c9af32569e8d9dc32aaa0840afa852
rasjidzz <mrisjads@gmail.com>
2024-04-22 17:23:02 +0700

    Update View Sementara Login Signup

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -6,34 +6,55 @@
       <div class="col-sm-4 text-center">
         <div class="row bg1">
           <h1>
-            Foto
+            Cardio Insight
           </h1>          
+          {{-- TAMBAHIN LOGO CARDIO INSIGHT --}}
         </div>
       </div>
-      {{-- <div class="col-sm-8 text-center">
-        <h1>
-          Login Page
-        </h1>
-      </div> --}}
-      {{-- <div class="container"> --}}
-        <div class="col-sm-8 text-center">
+      <div class="col-sm-8 text-center">
+        <div class="row">
           <h1 id="title">Login Page</h1>
+        </div>
+        <div class="row">
           <form id="loginForm" class="form">
             <!-- Login Form Fields -->
-            <input type="text" placeholder="Username">
-            <input type="password" placeholder="Password">
-            <button type="submit">Login</button>
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+            <button type="button" class="btn btn-primary">Login</button>
           </form>
           <form id="signupForm" class="form" style="display:none;">
             <!-- Signup Form Fields -->
-            <input type="text" placeholder="Username">
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
-            <button type="submit">Sign Up</button>
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="fullname" class="form-control" id="floatingInput" placeholder="fullname">
+              <label for="floatingInput">Full name</label>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="phonenum" class="form-control" id="floatingInput" placeholder="Phone Number">
+              <label for="floatingInput">Phone Number</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+            <button type="button" class="btn btn-primary">Login</button>
           </form>
-          <button id="toggleBtn">Switch to Sign Up</button>
+        </div>
+        {{-- <button id="toggleBtn">Switch to Sign Up</button> --}}
+        <div class="toggle-btn">
+          <button id="toggleBtn" class="btn btn-link btn-toggle">Switch to Sign Up</button>
         </div>
       </div>
+    </div>
   </div>
 
   <style>
@@ -58,26 +79,26 @@
     button {
       margin-top: 10px;
     }
-    </style>
+  </style>
 
-    <script>
-      document.getElementById('toggleBtn').addEventListener('click', function() {
-        var loginForm = document.getElementById('loginForm');
-        var signupForm = document.getElementById('signupForm');
-        var title = document.getElementById('title');
+  <script>
+    document.getElementById('toggleBtn').addEventListener('click', function() {
+      var loginForm = document.getElementById('loginForm');
+      var signupForm = document.getElementById('signupForm');
+      var title = document.getElementById('title');
 
-        if (loginForm.style.display === 'none') {
-          loginForm.style.display = 'block';
-          signupForm.style.display = 'none';
-          title.textContent = 'Login Page';
-          this.textContent = 'Switch to Sign Up';
-        } else {
-          loginForm.style.display = 'none';
-          signupForm.style.display = 'block';
-          title.textContent = 'Sign Up Page';
-          this.textContent = 'Switch to Login';
-        }
-      });
+      if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'block';
+        signupForm.style.display = 'none';
+        title.textContent = 'Login Page';
+        this.textContent = 'Switch to Sign Up';
+      } else {
+        loginForm.style.display = 'none';
+        signupForm.style.display = 'block';
+        title.textContent = 'Sign Up Page';
+        this.textContent = 'Switch to Login';
+      }
+    });
 
-    </script>
-@endsection()
+  </script>
+@endsection
