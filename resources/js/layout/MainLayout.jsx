cfef7c1e77c9d65c3ea4e7e5bf9fb34acef2fb08
Law1911 <epensusanto@gmail.com>
2024-06-06 14:13:22 +0700

    final bang tolong

--- resources/js/layout/MainLayout.jsx
+++ resources/js/layout/MainLayout.jsx
@@ -11,9 +11,20 @@ function MainLayout({ children, userName }) {
                 <div className="flex px-4 bg-custom-green justify-end rounded">
                     <div className="flex items-center justify-start w-full">
                         {/* Tambahkan judul dan isi di sini */}
-                        <div className="text-white w-full">
-                            <h2 className="text-lg font-semibold">Judul Konten</h2>
-                            <p>Isi konten yang menjelaskan tentang gambar hati.</p>
+                        <div className="text-white w-full px-4">
+                            <h1 className="text-[90px] font-semibold">
+                                Protect your cardiovascular
+                            </h1>
+                            <p className="text-sm">
+                                Penyakit kardiovaskular adalah kelompok kondisi
+                                yang melibatkan jantung dan pembuluh darah, yang
+                                dapat menyebabkan gangguan dalam aliran darah ke
+                                berbagai bagian tubuh. Kondisi-kondisi ini
+                                seringkali terjadi akibat penumpukan plak di
+                                dalam pembuluh darah (aterosklerosis), yang
+                                dapat menyebabkan penyempitan atau penyumbatan
+                                pembuluh darah
+                            </p>
                         </div>
                     </div>
                     <div className="">
--- resources/js/pages/homepage/Homepage.jsx
+++ resources/js/pages/homepage/Homepage.jsx
@@ -2,43 +2,27 @@ import React, { useState } from "react";
 import MainLayout from "../../layout/MainLayout";
 
 function HomePage() {
-    const userName = "Nama apa"; // Ganti dengan nama user sebenarnya
+    const userName = "Nama User"; // Ganti dengan nama user sebenarnya
     const [showForm1, setShowForm1] = useState(true);
     const [showForm2, setShowForm2] = useState(false);
     const [showForm3, setShowForm3] = useState(false);
-    const [showForm4, setShowForm4] = useState(false);
 
     const handleButtonClick = (buttonNumber) => {
         setShowForm1(false);
         setShowForm2(false);
         setShowForm3(false);
-        setShowForm4(false);
         if (buttonNumber === 1) {
             setShowForm1(true);
         } else if (buttonNumber === 2) {
             setShowForm2(true);
         } else if (buttonNumber === 3) {
             setShowForm3(true);
-        } else if (buttonNumber == 4) {
-            setShowForm4(true);
         }
     };
 
     const handleNextButtonClickForm2 = () => {
         setShowForm2(false);
         setShowForm3(true);
-    };
-
-    const handleNextButtonClickForm1 = () => {
-        setShowForm1(false);
-        setShowForm2(true);
-    };
-
-    const handleNextButtonClickForm3 = () => {
-        setShowForm1(false);
-        setShowForm2(false);
-        setShowForm3(false);
-        setShowForm4(true);
     };
 
     // Untuk Tombol yang ada di tengah dari 1 sampai 4
@@ -67,16 +51,13 @@ function HomePage() {
                     >
                         3
                     </button>
-                    <button
-                        className="w-12 h-12 bg-custom-green text-white rounded-full flex items-center justify-center"
-                        onClick={() => handleButtonClick(4)}
-                    >
-                        4
-                    </button>
                 </div>
 
                 {/* Ketika di tekan pada tombol 1 maka akan keluar form 1 yang ada di bawah ini */}
-                {showForm1 && (
+
+
+                {/* Ketika di tekan pada tombol 2 maka akan keluar form 2 yang ada di bawah ini */}
+                {showForm2 && (
                     <div className="bg-white p-6 rounded-lg mb-4 max-w-lg w-full">
                         <form>
                             <div className="mb-4">
@@ -95,21 +76,6 @@ function HomePage() {
                                     readOnly
                                 />
                             </div>
-                            <button
-                                type="button"
-                                onClick={handleNextButtonClickForm1}
-                                className="mt-2 bg-custom-green text-white py-2 px-4 rounded-full"
-                            >
-                                Next
-                            </button>
-                        </form>
-                    </div>
-                )}
-
-                {/* Ketika di tekan pada tombol 2 maka akan keluar form 2 yang ada di bawah ini */}
-                {showForm2 && (
-                    <div className="bg-white p-6 rounded-lg mb-4 max-w-lg w-full">
-                        <form>
                             <div className="mb-4">
                                 <label
                                     htmlFor="bloodPressure"
@@ -123,6 +89,22 @@ function HomePage() {
                                     name="bloodPressure"
                                     className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                     placeholder="Masukkan Tekanan Darah"
+                                    readOnly
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label
+                                    htmlFor="bloodPressure"
+                                    className="block font-sans font-semibold text-lg text-gray-700"
+                                >
+                                    Glucose
+                                </label>
+                                <input
+                                    type="text"
+                                    id="bloodPressure"
+                                    name="bloodPressure"
+                                    className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    placeholder="Mg/DL"
                                     readOnly
                                 />
                             </div>
@@ -143,37 +125,6 @@ function HomePage() {
                         <form>
                             <div className="mb-4">
                                 <label
-                                    htmlFor="bloodPressure"
-                                    className="block font-sans font-semibold text-lg text-gray-700"
-                                >
-                                    Glucose
-                                </label>
-                                <input
-                                    type="text"
-                                    id="bloodPressure"
-                                    name="bloodPressure"
-                                    className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    placeholder="Mg/DL"
-                                    readOnly
-                                />
-                            </div>
-                            <button
-                                type="button"
-                                onClick={handleNextButtonClickForm3}
-                                className="mt-2 bg-custom-green text-white py-2 px-4 rounded-full"
-                            >
-                                Next
-                            </button>
-                        </form>
-                    </div>
-                )}
-
-                {/* Ketika di tekan pada tombol 4 maka akan keluar form 4 yang ada di bawah ini */}
-                {showForm4 && (
-                    <div className="bg-white p-6 rounded-lg mb-4 max-w-lg w-full">
-                        <form>
-                            <div className="mb-4">
-                                <label
                                     htmlFor="height"
                                     className="block font-sans font-semibold text-lg text-gray-700"
                                 >
@@ -189,88 +140,88 @@ function HomePage() {
                                 <label
                                     htmlFor="weight"
                                     className="block font-sans font-semibold text-lg text-gray-700 mt-5"
+                                    >
+                                        Weight
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="weight"
+                                        name="weight"
+                                        className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        placeholder="weight (kg)"
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label
+                                        htmlFor="smoke"
+                                        className="block font-sans font-semibold text-lg text-gray-700"
+                                    >
+                                        Do you smoke?
+                                    </label>
+                                    <select
+                                        id="smoke"
+                                        name="smoke"
+                                        className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    >
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </div>
+                                <div className="mb-4">
+                                    <label
+                                        htmlFor="sport"
+                                        className="block font-sans font-semibold text-lg text-gray-700"
+                                    >
+                                        Are you active in sports or activities?
+                                    </label>
+                                    <select
+                                        id="sport"
+                                        name="sport"
+                                        className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    >
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </div>
+                                <div className="mb-4">
+                                    <label
+                                        htmlFor="alcohol"
+                                        className="block font-sans font-semibold text-lg text-gray-700"
+                                    >
+                                        Do you consume alcohol?
+                                    </label>
+                                    <select
+                                        id="alcohol"
+                                        name="alcohol"
+                                        className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    >
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </select>
+                                </div>
+                                <button
+                                    type="button"
+                                    onClick=""
+                                    className="mt-2 bg-custom-green text-white py-2 px-4 rounded-full"
                                 >
-                                    Weight
-                                </label>
-                                <input
-                                    type="text"
-                                    id="weight"
-                                    name="weight"
-                                    className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    placeholder="weight (kg)"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="smoke"
-                                    className="block font-sans font-semibold text-lg text-gray-700"
-                                >
-                                    Do you smoke?
-                                </label>
-                                <select
-                                    id="smoke"
-                                    name="smoke"
-                                    className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                >
-                                    <option value="yes">Yes</option>
-                                    <option value="no">No</option>
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="sport"
-                                    className="block font-sans font-semibold text-lg text-gray-700"
-                                >
-                                    Are you active in sports or activities?
-                                </label>
-                                <select
-                                    id="sport"
-                                    name="sport"
-                                    className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                >
-                                    <option value="yes">Yes</option>
-                                    <option value="no">No</option>
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="alcohol"
-                                    className="block font-sans font-semibold text-lg text-gray-700"
-                                >
-                                    Do you consume alcohol?
-                                </label>
-                                <select
-                                    id="alcohol"
-                                    name="alcohol"
-                                    className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                >
-                                    <option value="yes">Yes</option>
-                                    <option value="no">No</option>
-                                </select>
-                            </div>
-                            <button
-                                type="button"
-                                onClick=""
-                                className="mt-2 bg-custom-green text-white py-2 px-4 rounded-full"
-                            >
-                                Go to see result
+                                    Go to see result
+                                </button>
+                            </form>
+                        </div>
+                    )}
+
+                    {/* Berguna untuk memunculkan tombol START pada form 1 ,2 , dan 3 */}
+                    {(showForm1) && (
+                        <div className="pt-12">
+                            <button className=" animate-pulse w-60 h-60 text-custom-green text-[40px] shadow-lg font-bold font-sans rounded-full flex items-center justify-center border-4 border-custom-green">
+                                START
                             </button>
-                        </form>
-                    </div>
-                )}
+                        </div>
+                    )}
+                </div>
+            </MainLayout>
+        );
+    }
 
+    export default HomePage;
 
-                {/* Berguna untuk memunculkan tombol START pada form 1 ,2 , dan 3 */}
-                {(showForm1 || showForm2 || showForm3) && (
-                    <div>
-                        <button className=" animate-pulse w-60 h-60 text-custom-green text-[40px] shadow-lg font-bold font-sans rounded-full flex items-center justify-center border-4 border-custom-green">
-                            START
-                        </button>
-                    </div>
-                )}
-            </div>
-        </MainLayout>
-    );
-}
-
-export default HomePage;
